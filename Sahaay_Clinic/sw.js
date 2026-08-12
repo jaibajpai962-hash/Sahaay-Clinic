b5fe15820471ac3cb5dacec111dce8bde4d5e281
princeomar-AIdeep <princeomar0512@gmail.com>
2026-08-12 18:47:49 +0530

    Delete Sahaay_Clinic/sw.js

--- Sahaay_Clinic/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="sahaay-clinic-v1";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>cached))));
